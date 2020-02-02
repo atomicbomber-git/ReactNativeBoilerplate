00952e82b1a2fa57cc94b8a8f88e591b474fafa9
James Patrick Keegan <atomicbomber.email@gmail.com>
2020-02-02 12:48:05 +0700

    Updated.

--- App.tsx
+++ App.tsx
@@ -1,78 +1,76 @@
 import React, {useEffect} from 'react';
-import {StyleSheet, Text,} from 'react-native';
-
-import {Colors,} from 'react-native/Libraries/NewAppScreen';
-import {GoogleSignin} from "@react-native-community/google-signin";
-import {serverWebClientId} from "./config";
+import {Text, StyleSheet, View} from 'react-native';
+import {GoogleSignin, GoogleSigninButton} from "@react-native-community/google-signin";
+import {development as config} from "./config";
 
 GoogleSignin.configure({
-    webClientId: serverWebClientId, // client ID of type WEB for your server (needed to verify user ID and offline access)
-    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+    webClientId: config.serverWebClientId,
+    offlineAccess: true
 });
 
-async function signIn() {
-    await GoogleSignin.hasPlayServices();
-    return await GoogleSignin.signIn();
+async function signInWithGoogle() {
+    try {
+        return await GoogleSignin.signInSilently()
+    }
+    catch (error) {
+        if (!await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true })) {
+            throw new Error("Error: Google Play Services isn't available.");
+        }
+
+         return await GoogleSignin.signIn();
+    }
+}
+
+async function signInWithServer(authData) {
+    const response = await fetch(`${config.serverHost}/api/socialite`, {
+        method: 'POST',
+        headers: { 'ContentType': 'application/json' },
+        body: JSON.stringify({
+            code: authData.serverAuthCode
+        })
+    })
+
+    return await response.json()
 }
 
 const App = () => {
+    async function signInAsync() {
+        const authData = await signInWithGoogle()
 
-    useEffect(() => {
-        signIn()
+        console.info(authData)
+
+        return "TEST"
+        // return signInWithServer(authData)
+    }
+
+    function signIn() {
+        signInAsync()
             .then(authData => {
                 console.info(authData)
             })
             .catch(error => {
                 console.error(error)
             })
-    }, [])
+    }
 
     return (
         <>
-            <Text>
-                This app tries to log in
-            </Text>
+            <View style={styles.home}>
+
+                <GoogleSigninButton
+                    onPress={() => { signIn() }}
+                />
+            </View>
         </>
     );
 };
 
 const styles = StyleSheet.create({
-    scrollView: {
-        backgroundColor: Colors.lighter,
-    },
-    engine: {
-        position: 'absolute',
-        right: 0,
-    },
-    body: {
-        backgroundColor: Colors.white,
-    },
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-        color: Colors.black,
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-        color: Colors.dark,
-    },
-    highlight: {
-        fontWeight: '700',
-    },
-    footer: {
-        color: Colors.dark,
-        fontSize: 12,
-        fontWeight: '600',
-        padding: 4,
-        paddingRight: 12,
-        textAlign: 'right',
-    },
-});
+    home: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
 
 export default App;
--- config.ts
+++ config.ts
@@ -1,1 +1,10 @@
-export const serverWebClientId = '1086103525793-6s5k38kiak0us9p9ha7rpabj3uumbf15.apps.googleusercontent.com';
+
+export const development = {
+    serverWebClientId: '1086103525793-6s5k38kiak0us9p9ha7rpabj3uumbf15.apps.googleusercontent.com',
+    serverHost: 'http://10.0.2.2:8000',
+}
+
+export const production = {
+    serverWebClientId: '1086103525793-6s5k38kiak0us9p9ha7rpabj3uumbf15.apps.googleusercontent.com',
+    serverHost: 'http://localhost:8000',
+}
